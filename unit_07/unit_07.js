082cb04dfab37322a2bae13ca5292a1392584734
GarifullinII <ildaridrisovich@gmail.com>
2021-09-08 16:06:15 +0400

    add task 6

--- unit_07/unit_07.js
+++ unit_07/unit_07.js
@@ -77,7 +77,7 @@ document.querySelector('.b-5').onclick = function () {
 // Напишите функцию t6, которая принимает 2 числа и возвращает случайное целое число от первого до второго принятого параметра. Внимание, это первая задача, решение которой нужно найти в google. Не пишите решение сами!
 
 function t6(a, b) {
-
+    return Math.random()*(a - b) + b;
 }
 
 document.querySelector('.b-6').onclick = function () {
