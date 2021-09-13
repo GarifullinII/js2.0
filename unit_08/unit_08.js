fadfe4aeb7f78040dfba92f1a94902ce050fdc20
GarifullinII <ildaridrisovich@gmail.com>
2021-09-14 00:01:23 +0400

    add task 12

--- unit_08/unit_08.js
+++ unit_08/unit_08.js
@@ -260,9 +260,15 @@ document.querySelector('.b-11').onclick = t11;
 // перебрать их с помощью цикла  while. Обращение к div выглядит так elem[i]
 // применить к каждому elem[i].style.background = 'orange'
 
-
 function t12() {
+	let elem = document.querySelectorAll('.div-12');
+	let b = '';
+	let i = 0;
 
+	while ( i < elem.length) {
+		elem[i].style.background = 'orange';
+		i++;
+	}
 }
 
 document.querySelector('.b-12').onclick = t12;
