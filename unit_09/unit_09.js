9c1be4f111228c59ddfe1dbea22c1c0230d7890f
GarifullinII <ildaridrisovich@gmail.com>
2021-09-16 22:19:27 +0400

    add task 12 and change oder tasks

--- unit_09/unit_09.js
+++ unit_09/unit_09.js
@@ -147,15 +147,9 @@ while ( i < div10.length ) {
 
 function f11() {
     let a = document.querySelector('.out-11');
-    let b = document.createElement('div'); 
+    let b = document.createElement('div');
 
-    if (f11.isShow) {
-        return false;
-    }
-
-    f11.isShow = true;
-
-    b.innerHTML = 25;
+    b.innerHTML = '25';
     a.append(b);
 }
 
@@ -164,9 +158,15 @@ document.querySelector('.b-11').onclick = f11;
 //  Task 12
 // Добавьте кнопку .b-12, которая запускает функцию f12. Функция создает через createElement div c текстом 12 и добавляет ему класс bg-12. Созданный div добавляется в out-12.
 
-
 function f12() {
 
+    let div12 = document.createElement('div');
+    div12.innerHTML = '12';
+    div12.classList.add('bg-12');
+
+    let out12 = document.querySelector('.out-12');
+
+    out12.append(div12);
 }
 
 document.querySelector('.b-12').onclick = f12;
