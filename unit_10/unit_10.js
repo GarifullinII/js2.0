2c2571295e13f1868d7b9e6220436c5c046f7158
GarifullinII <ildaridrisovich@gmail.com>
2021-09-17 15:24:08 +0400

    add task 2

--- unit_10/unit_10.js
+++ unit_10/unit_10.js
@@ -14,20 +14,25 @@ function f1() {
 
 document.querySelector('.b-1').onclick = f1;
 
+
 // Task 2
 //Создайте массив ar2 содержащий строки, числа, булевы значения. Выведите его в div.out-2. Используйте шаблон вывода из кода в JS. Вывод - по нажатию кнопки b-2
 
+let ar2 = ['Outlander', 'V=2.0l', 300, false];
 // let ar2 = // переменную обьявляем здесь!!!!
 
 function f2() {
-    // let out = '';
-    // for (let i = 0; i < ar2.length; i++) {
-    //     out +=arr2[i]+' ';
-    // }
-    // document.querySelector('.out-2').innerHTML = out;
+    let out = '';
+
+    for (let i = 0; i < ar2.length; i++) {
+        out += ar2[i] + ' ';
+    }
+
+    document.querySelector('.out-2').innerHTML = out;
 }
 
 document.querySelector('.b-2').onclick = f2;
+
 
 // Task 3
 //Создайте массив ar3, заполните его любыми значениями. Выведите длину массива.
