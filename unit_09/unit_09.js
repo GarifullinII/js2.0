f73e02ff157559ead424830ca8424fb645c9084f
GarifullinII <ildaridrisovich@gmail.com>
2021-09-17 12:27:15 +0400

    add task 18

--- unit_09/unit_09.js
+++ unit_09/unit_09.js
@@ -256,7 +256,9 @@ document.querySelector('.b-17').onclick = f17;
 // Кнопкa .b-18, которая запускает функцию f18. Функция с помощью getAttribute получает data-b атрибут с параграф .p-18 и выводит в .out-18.
 
 function f18() {
-
+    let out18 = document.querySelector('.out-18');
+    let p18 = document.querySelector('.p-18');
+    out18.innerHTML = p18.getAttribute('data-b');
 }
 
 document.querySelector('.b-18').onclick = f18;
