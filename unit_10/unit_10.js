919126b694b22cdc1e5056380d765ee44ea534fe
GarifullinII <ildaridrisovich@gmail.com>
2021-09-20 00:20:42 +0400

    add task 17

--- unit_10/unit_10.js
+++ unit_10/unit_10.js
@@ -373,7 +373,16 @@ document.querySelector('.b-16').onclick = f16;
 let ar17 = [3, 0, 2, 6, 0, 1, 3, 1, 9, 0, 2, 0];
 
 function f17() {
+    let out17 = document.querySelector('.out-17');
 
+    let val = 0;
+    for ( let i = 0; i < ar17.length; i++ ) {
+        if (ar17[i] > 3) {
+            val++;
+        }
+    }
+
+    out17.innerHTML = val;
 }
 
 document.querySelector('.b-17').onclick = f17;
