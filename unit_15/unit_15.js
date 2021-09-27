42f8966fd0ac22c8c86ef7422794b6d012a87400
GarifullinII <ildaridrisovich@gmail.com>
2021-09-27 20:50:56 +0400

    add task 2

--- unit_15/unit_15.js
+++ unit_15/unit_15.js
@@ -18,10 +18,16 @@ console.log(s1);
 // Task 2
 // При нажатии b-2 выполняете функцию f2. Функция должна добавить в набор s2 элементы, которые пользователь вводит в i-2. Функция должна выводить в консоль s2 после каждого добавления элемента.
 
-
+let s2 = new Set();
 // let s2 =
 
-const f2 = () => { }
+const f2 = () => {
+    let i2 = document.querySelector('.i-2').value;
+
+    s2.add(i2);
+
+    console.log(s2);
+}
 
 document.querySelector('.b-2').onclick = f2;
 
