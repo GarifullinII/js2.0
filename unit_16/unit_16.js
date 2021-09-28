464645521d1d05f7d2f415c24328216aa023fe63
GarifullinII <ildaridrisovich@gmail.com>
2021-09-28 22:26:35 +0400

    add task 12

--- unit_16/unit_16.js
+++ unit_16/unit_16.js
@@ -224,7 +224,15 @@ document.querySelector('.b-11').addEventListener('click', f11);
 let a12 = [4,5,6,7];
 
 function f12() {
+    let out12 = document.querySelector('.out-12');
 
+    let out = '';
+
+    for (let item of a12) {
+        out += `${item} `;
+    }
+
+    out12.innerHTML = out;
 }
 
 document.querySelector('.b-12').addEventListener('click', f12);
