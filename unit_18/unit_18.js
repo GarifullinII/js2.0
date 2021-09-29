187ff1c65bb6f8dfc4773617e34ecc75c2b7068b
GarifullinII <ildaridrisovich@gmail.com>
2021-09-29 21:28:09 +0400

    add task 7

--- unit_18/unit_18.js
+++ unit_18/unit_18.js
@@ -108,8 +108,14 @@ document.querySelector('.b-6').onclick = t6;
 // Task 7 ============================================
 /*  Дана строка str7='hello world hi mazai' - преобразуйте ее в массив и разбейте по словам. Причем слова не должны содержать пробелов и присвойте a6_res. выведите на страницу.  Запускаться решение должно при вызове функции t7. */
 
-function t7() {
+str7='hello world hi mazai';
 
+function t7() {
+    a7_res = str7.split(' ');
+
+    console.log(a7_res);
+
+    document.querySelector('.out-7').innerHTML = a7_res;
 }
 
 document.querySelector('.b-7').onclick = t7;
