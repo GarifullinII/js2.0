54c10de38e6144e5911e4174da297d16e2bafd19
GarifullinII <ildaridrisovich@gmail.com>
2021-09-29 21:39:23 +0400

    add task 8

--- unit_18/unit_18.js
+++ unit_18/unit_18.js
@@ -123,8 +123,14 @@ document.querySelector('.b-7').onclick = t7;
 // Task 8 ============================================
 /*  Дан массив a8 = [1,2,66,77,15] - преобразуйте ее в строку. Разделитель - дефис. Результат присвойте a8_res. Запускаться решение должно при вызове функции t8. */
 
-function t8() {
+a8 = [1,2,66,77,15];
 
+function t8() {
+    a8_res = a8.join('-');
+
+    console.log(a8_res);
+
+    document.querySelector('.out-8').innerHTML = a8_res;
 }
 
 document.querySelector('.b-8').onclick = t8;
