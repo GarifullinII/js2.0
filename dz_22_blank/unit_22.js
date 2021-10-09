a53df6cbdf57462dcd08c6b907b2aee0bf0f899b
GarifullinII <ildaridrisovich@gmail.com>
2021-10-09 19:45:14 +0400

    add task 2

--- dz_22_blank/unit_22.js
+++ dz_22_blank/unit_22.js
@@ -22,8 +22,13 @@ document.querySelector('.b-1').onclick = t1;
 function t2() {
     let a = 4;
     let b = 5;
-    document.querySelector('.out-2222222').innerHTML = a*b;
 
+    try {
+        document.querySelector('.out-2222222').innerHTML = a * b;
+    }
+    catch (err) {
+        document.querySelector('.out-2').innerHTML = a * b;
+    }
 }
 
 document.querySelector('.b-2').onclick = t2;
