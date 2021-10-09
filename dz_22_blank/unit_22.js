4d8dedea1a7690788eaaba61175a55c73d52ed8f
GarifullinII <ildaridrisovich@gmail.com>
2021-10-09 19:34:26 +0400

    add task 1

--- dz_22_blank/unit_22.js
+++ dz_22_blank/unit_22.js
@@ -1,12 +1,16 @@
 //  Task 1
 // Добавьте в код функции try catch так, чтобы вместо ошибки выводилось в out-1 цифра 1.
 
-
 function t1() {
     // тут добавляете try
-    let a = 22;
-    let c = a + d;
+    try {
+        let a = 22;
+        let c = a + d;
+    }
     // тут catch
+    catch (err) {
+        document.querySelector('.out-1').textContent = 1;
+    }
     // .. и вывод
 }
 
