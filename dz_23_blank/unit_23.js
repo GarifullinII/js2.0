f0b7050eef08a296991ca3ec7712df8ed25ac090
GarifullinII <ildaridrisovich@gmail.com>
2021-10-10 22:06:27 +0400

    add task 7

--- dz_23_blank/unit_23.js
+++ dz_23_blank/unit_23.js
@@ -86,10 +86,17 @@ document.querySelector('.b-6').onclick = t6;
 // Task 7 ============================================
 /*  Создайте input i-7 куда пользователь может вводить числа и строки. Создайте массив a7. Когда пользователь нажимает кнопку b-7 число должно добавляться в массив. Массив должен сохраняться в LS с ключем a7.*/
 
-function t7() {
+let a7 = [];
 
+function t7() {
+    let val = document.querySelector('.i-7').value;
+
+    a7.push(val);
+
+    localStorage.setItem('a7', JSON.stringify(a7));
 }
 
+document.querySelector('.b-7').onclick = t7;
 // ваше событие здесь!!!
 
 
