ceb2b798fb2d742edba099785baf6991023e3be9
GarifullinII <ildaridrisovich@gmail.com>
2021-10-13 02:32:44 +0400

    add task 14

--- dz_25_blank/dz_25_blank/unit_25.js
+++ dz_25_blank/dz_25_blank/unit_25.js
@@ -304,9 +304,24 @@ document.querySelector('.b-13').onclick = t13;
 /*  Отправьте POST запрос на сайт http://getpost.itgid.info/index2.php. В качестве action укажите 5. Если все сделано верно, сервер вернет текущее время и дату. Не забывайте указывать параметр auth (ключ в чате). Выведите в out-14 результат. Запускаться функция должна по нажатию b-14.*/
 
 function t14() {
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            myFunction14(this.responseText)
+        }
+    }
 
+    xhttp.open('POST', `http://getpost.itgid.info/index2.php?auth=${auth}&action=5`, true);
+    
+    xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    xhttp.send(`auth=${auth}&action=5`);
 }
 
+function myFunction14(data) {
+    document.querySelector('.out-14').innerHTML = data;
+}
+
+document.querySelector('.b-14').onclick = t14;
 // ваше событие здесь!!!
 
 // Task 15============================================
